c7c8b2cb37eb4eaab5d14df400694dd6d5c30814
Android Studio <devandroid@ort>
2026-05-06 08:21:58 -0300

    termine

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,13 +18,14 @@ public class HomeController : Controller
         return View();
     }
     [HttpPost] 
-  public IActionResult Solicitud(string nombre, int edad, int dni, int trabaja, int tipoempleo, int ingreso, int deudas, bool credito, bool bancario, bool informal, int monto, int plazo, bool terminos)
+  public IActionResult Solicitud(string nombre, int edad, int dni, int trabaja, int tipoempleo, int ingreso, int deudas, bool credito, bool bancario, bool informal, int monto, int plazo, string terminos)
     {   bool aprobado;
-        if (edad < 18 || trabaja == 2 || tipoempleo == 4 || ingreso < 250000 || monto < ingreso*5 || deudas == 1 || !terminos)
+
+        if (edad < 18 || trabaja == 2 || tipoempleo == 4 || ingreso < 250000 || monto > ingreso/5 || deudas == 1 || terminos != "on")
         {
             aprobado = false;
         }
-        else{aprobado = true;}
+        else aprobado = true;
         ViewBag.estado = aprobado;
 
         return View("infoSolicitud");
